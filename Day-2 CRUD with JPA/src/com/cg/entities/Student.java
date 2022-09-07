9a79d08eef1f6f57c39ad7c32692746bef8a0c18
USER <USER@DESKTOP-2R3Q0AU>
2022-09-07 21:55:26 +0530

    first commit

---
+++ Day-2 CRUD with JPA/src/com/cg/entities/Student.java
@@ -0,0 +1,34 @@
+package com.cg.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Student")
+public class Student implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private int rollno;
+	private String name;
+	
+	//Getters and setters method
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
